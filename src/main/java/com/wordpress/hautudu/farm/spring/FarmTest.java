5d839bfa643d1343173ae5d35b6d83ff179b0c30
Nghia Nguyen <tunghiabt2010@gmail.com>
2017-07-29 20:51:31 +0700

    Initializing the Spring container

--- src/main/java/com/wordpress/hautudu/farm/spring/FarmTest.java
+++ src/main/java/com/wordpress/hautudu/farm/spring/FarmTest.java
@@ -1,17 +1,40 @@
 package com.wordpress.hautudu.farm.spring;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 public class FarmTest {
     public static void main(String[] args) {
-	ApplicationContext context = new ClassPathXmlApplicationContext("beanft.xml");
+	// Application context
+	// ApplicationContext context = new ClassPathXmlApplicationContext("beanft.xml");//ok
+	ApplicationContext context = new FileSystemXmlApplicationContext("/src/main/resources/beanft.xml");
+
 	BeanFactory factory = context;
 	House house = (House) factory.getBean("houseBean");
 
 	System.out.println("Animal 1: " + house.getAnimal1().makeSound());
 	System.out.println("Animal 2: " + house.getAnimal2().makeSound());
+	System.out.println(Arrays.asList(context.getBeanDefinitionNames()));
+
+	// -----------------------------Bean factories---------------------
+	Resource resource1 = new FileSystemResource("/Users/nghiant/Code/SpringDemo/springbase/AnimalFarmAutoWiring/src/main/resources/beanft.xml");
+	BeanFactory beanFactory1 = new XmlBeanFactory(resource1);
+	House house2 = (House) beanFactory1.getBean("houseBean");
+	System.out.println("Animal 1: " + house2.getAnimal1().makeSound());
+	System.out.println("Animal 2: " + house2.getAnimal2().makeSound());
+
+	Resource resource2 = new ClassPathResource("beanft.xml");
+	BeanFactory beanFactory2 = new XmlBeanFactory(resource2);
+	House house3 = (House) beanFactory2.getBean("houseBean");
+	System.out.println("Animal 1: " + house3.getAnimal1().makeSound());
+	System.out.println("Animal 2: " + house3.getAnimal2().makeSound());
     }
 
 }
--- src/main/java/com/wordpress/hautudu/farm/spring/annotation/FarmTest.java
+++ src/main/java/com/wordpress/hautudu/farm/spring/annotation/FarmTest.java
@@ -1,23 +1,33 @@
 package com.wordpress.hautudu.farm.spring.annotation;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class FarmTest {
     public static void main(String[] args) {
-	// ApplicationContext context = new ClassPathXmlApplicationContext("beanft.xml");
 	ApplicationContext context = new AnnotationConfigApplicationContext(House.class);
 	@SuppressWarnings("unused")
 	BeanFactory factory = context;
 	House house = (House) factory.getBean("house");
-	// Cat cat = (Cat) factory.getBean("catBean");
-	// System.out.println(cat.makeSound());
-	// Dog dog = (Dog) factory.getBean("dogBean");
-	// System.out.println(dog.makeSound());
+	Cat cat = (Cat) factory.getBean("cat");
+	System.out.println(cat.makeSound());
+	Dog dog = (Dog) factory.getBean("dog");
+	System.out.println(dog.makeSound());
 
 	System.out.println("Animal 1: " + house.getAnimal1().makeSound());
 	System.out.println("Animal 2: " + house.getAnimal2().makeSound());
+	System.out.println(Arrays.asList(context.getBeanDefinitionNames()));
+	ApplicationContext context2 = new AnnotationConfigApplicationContext(House2.class);
+	BeanFactory factory2 = context2;
+	House2 house2 = (House2) factory2.getBean("house2");
+	System.out.println("Animal 1: " + house2.getAnimal1().makeSound());
+	System.out.println("Animal 2: " + house2.getAnimal2().makeSound());
+	System.out.println(Arrays.asList(context2.getBeanDefinitionNames()));
+	Dog dog2 = (Dog) factory2.getBean("dog");
+	System.out.println(dog2.makeSound());
     }
 
 }
