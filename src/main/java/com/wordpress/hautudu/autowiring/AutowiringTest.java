3a0f713b69f5f18cb3a6f156997579537040fb0a
Nghia Nguyen <tunghiabt2010@gmail.com>
2017-07-29 16:40:56 +0700

    update code

--- src/main/java/com/wordpress/hautudu/autowiring/AutowiringTest.java
+++ src/main/java/com/wordpress/hautudu/autowiring/AutowiringTest.java
@@ -8,17 +8,17 @@ public class AutowiringTest {
     public static void main(String[] args) {
 	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
 	BeanFactory beanFactory = applicationContext;
-	LaptopStore laptopStore = (LaptopStore) beanFactory.getBean("laptopStore");
-	LaptopStore2 laptopStore2 = (LaptopStore2) beanFactory.getBean("laptopStore2");
-	LaptopStore3 laptopStore3 = (LaptopStore3) beanFactory.getBean("laptopStore3");
+	LaptopStore laptopStorebyName = (LaptopStore) beanFactory.getBean("laptopStore");
+	LaptopStore2 laptopStoreByType = (LaptopStore2) beanFactory.getBean("laptopStore2");
+	LaptopStore3 laptopStoreByContructor = (LaptopStore3) beanFactory.getBean("laptopStore3");
 	LaptopStore4 laptopStore4 = (LaptopStore4) beanFactory.getBean("laptopStore4");
 
-	System.out.println("Laptop1's trade mark: " + laptopStore.getLaptop1().showTradeMark());
-	System.out.println("Laptop2's trade mark: " + laptopStore.getLaptop2().showTradeMark());
-	System.out.println("Laptop1's trade mark2: " + laptopStore2.getLaptop1().showTradeMark());
-	System.out.println("Laptop2's trade mark2: " + laptopStore2.getLaptop2().showTradeMark());
-	System.out.println("Laptop1's trade mark3: " + laptopStore3.getLaptop1().showTradeMark());
-	System.out.println("Laptop2's trade mark3: " + laptopStore3.getLaptop2().showTradeMark());
+	System.out.println("Laptop1's trade mark: " + laptopStorebyName.getLaptop1().showTradeMark());
+	System.out.println("Laptop2's trade mark: " + laptopStorebyName.getLaptop2().showTradeMark());
+	System.out.println("Laptop1's trade mark2: " + laptopStoreByType.getLaptop1().showTradeMark());
+	System.out.println("Laptop2's trade mark2: " + laptopStoreByType.getLaptop2().showTradeMark());
+	System.out.println("Laptop1's trade mark3: " + laptopStoreByContructor.getLaptop1().showTradeMark());
+	System.out.println("Laptop2's trade mark3: " + laptopStoreByContructor.getLaptop2().showTradeMark());
 	System.out.println("Laptop1's trade mark4: " + laptopStore4.getLaptop1().showTradeMark());
 	System.out.println("Laptop2's trade mark4: " + laptopStore4.getLaptop2().showTradeMark());
     }
